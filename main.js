65fef81d9a9b1e9b9012826b8369fde9b36f268f
viggoStrom <viggostrom05@gmail.com>
2024-08-25 19:36:36 +0200

    electron boilerplate

---
+++ main.js
@@ -0,0 +1,29 @@
+
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+
+    const win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        fullscreen: true,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
+
+    win.loadFile("html/index.html");
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
