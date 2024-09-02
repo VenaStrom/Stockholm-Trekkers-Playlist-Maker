508c3bc451fb2bd372ce5eba3d32fbf2bdbce0e8
viggoStrom <viggostrom05@gmail.com>
2024-09-02 21:50:52 +0200

    download technically works

--- scripts/downloadAssets.js
+++ scripts/downloadAssets.js
@@ -2,6 +2,9 @@ const { BrowserWindow, session } = require("electron");
 const fs = require("fs");
 const path = require("node:path")
 
+const downloadStatus = {
+    "fileCount": 0,
+}
 
 const downloadPauses = (force = false) => {
 
@@ -11,7 +14,7 @@ const downloadPauses = (force = false) => {
         console.log("force downloading...");
         fs.rmSync(videoFolder, { recursive: true });
     } else {
-        console.log("downloading...");
+        console.log("looking for files...");
     }
 
     if (!fs.existsSync(videoFolder)) {
@@ -19,10 +22,22 @@ const downloadPauses = (force = false) => {
     }
 
     const fileIDs = JSON.parse(fs.readFileSync(path.join(__dirname, "fileIDs.json")))
+    const videos = fileIDs.videos;
+    let index = 0;
 
-    fileIDs.videos.forEach((fileID) => {
+    const endOfDownload = () => {
+        if (index + 1 < videos.length) {
+            index++;
+            getVideo(videos[index]);
+        } else {
+            console.log("all videos downloaded");
+        }
+    };
+
+    const getVideo = (fileID) => {
         if (fs.existsSync(videoFolder + fileID.name) && !force) {
             console.log(fileID.name + " already downloaded");
+            endOfDownload();
             return;
         } else {
             console.log("downloading " + fileID.name);
@@ -33,8 +48,8 @@ const downloadPauses = (force = false) => {
         });
 
         session.defaultSession.on("will-download", (event, item, webContents) => {
-            item.setSavePath(videoFolder + item.getFilename());
-            // item.setSavePath(videoFolder + fileID.name);
+            // item.setSavePath(videoFolder + item.getFilename());
+            item.setSavePath(videoFolder + fileID.name);
 
             item.on("updated", (event, state) => {
                 if (state === "interrupted") {
@@ -44,8 +59,8 @@ const downloadPauses = (force = false) => {
                         console.log(`download of ${fileID.name} is paused`);
                     } else {
                         console.log(`${fileID.name} received ${(item.getReceivedBytes() / item.getTotalBytes() * 100).toFixed(0)}% ${(item.getReceivedBytes() / 1024 / 1024).toFixed(0)} MB`);
-                    }
-                }
+                    };
+                };
             });
 
             item.once("done", (event, state) => {
@@ -53,18 +68,26 @@ const downloadPauses = (force = false) => {
                     console.log(`download of ${fileID.name} completed successfully`);
                 } else {
                     console.log(`download failed with state: ${state}`);
-                }
+                };
+
+                if (index + 1 < videos.length) {
+                    index++;
+                    getVideo(videos[index]);
+                } else {
+                    console.log("all videos downloaded");
+                };
                 downloadWindow.close();
             });
         });
-
 
         downloadWindow.loadURL(fileIDs.urlTemplate + fileID.id);
 
         downloadWindow.webContents.on("did-finish-load", () => {
             downloadWindow.webContents.executeJavaScript(`try{document.getElementById("uc-download-link").click();} catch (error) {console.log(error);}`);
         });
-    });
-}
+    };
+
+    getVideo(videos[index]);
+};
 
 module.exports = { downloadPauses };
