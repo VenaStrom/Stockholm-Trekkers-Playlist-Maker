aa09bfa2261433e0fe8d7b8c6cf825d7a2170fe1
viggoStrom <viggostrom05@gmail.com>
2024-09-11 17:00:48 +0200

    more renaming

--- scripts/load-projects/overview.js
+++ scripts/load-projects/overview.js
@@ -5,5 +5,11 @@ const path = require("node:path");
 ipcMain.handle("get-projects", async () => {
     const projects = [];
 
-    const projectFolder = path.join(__dirname, "");
+    const projectFolder = path.join(__dirname, "../../user-data/projects/");
+
+    if (!fs.existsSync(projectFolder)) {
+        fs.mkdirSync(projectFolder, { recursive: true });
+
+        return false;
+    };
 });
