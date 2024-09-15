e38768f43882f41bdcd82932b83c6f4cdd88a0e0
viggoStrom <viggostrom05@gmail.com>
2024-09-15 11:37:40 +0200

    make id

--- html/js/project-editor/idCheck.js
+++ html/js/project-editor/idCheck.js
@@ -1,9 +1,23 @@
 
-const pageURL = window.location.href;
-const arguments = {};
-pageURL.split("?")[1].split("&").forEach((arg) => {
-    const [key, value] = arg.split("=");
+const pageBornAt = new Date().getTime();
 
-    arguments[key] = value;
-});
 
+const generateID = () => {
+    const pageURL = window.location.href;
+    const arguments = {};
+    pageURL.split("?")[1].split("&").forEach((arg) => {
+        const [key, value] = arg.split("=");
+
+        arguments[key] = value;
+    });
+
+    if (arguments.id !== "new") {
+        return arguments.id;
+    };
+
+    const time = new Date().getTime();
+
+    const id = time.toString() + pageBornAt.toString();
+    
+    console.log(id);
+}
