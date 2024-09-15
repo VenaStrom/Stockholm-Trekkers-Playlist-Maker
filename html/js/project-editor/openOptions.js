528ba1b6956859a640cc2239348687f00270e60d
viggoStrom <viggostrom05@gmail.com>
2024-09-15 13:13:08 +0200

    block structure

---
+++ html/js/project-editor/openOptions.js
@@ -0,0 +1,13 @@
+
+const expandOptions = (source) => {
+    const options = source.parentElement.querySelector('.options');
+    const state = options.style.display;
+
+    if (state === 'flex') {
+        source.textContent = "Options ▼";
+        options.style.display = 'none';
+    } else {
+        source.textContent = "Options ▲";
+        options.style.display = 'flex';
+    };
+};
