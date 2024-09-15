c1d6fc88618202d87b8432958bd9f978a5299ad2
viggoStrom <viggostrom05@gmail.com>
2024-09-15 13:55:23 +0200

    easier option choosing

--- html/js/project-editor/openOptions.js
+++ html/js/project-editor/openOptions.js
@@ -24,3 +24,22 @@ optionButtons.forEach(button => {
         };
     });
 });
+
+
+const checkCheckbox = (source) => {
+    const checkbox = source.querySelector("input[type='checkbox']");
+    checkbox.checked = !checkbox.checked;
+};
+
+const checkboxAreas = document.querySelectorAll(".block .options div div");
+
+checkboxAreas.forEach(checkbox => {
+    checkbox.addEventListener("click", () => {
+        checkCheckbox(checkbox);
+    });
+    checkbox.addEventListener("keydown", (event) => {
+        if (event.key === "Enter" || event.key === " ") {
+            checkCheckbox(checkbox);
+        };
+    });
+});
