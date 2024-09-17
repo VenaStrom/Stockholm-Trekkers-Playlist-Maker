bbba76fcb6d2d4bac15b68e7fc1a4294f141e823
viggoStrom <viggostrom05@gmail.com>
2024-09-17 18:38:22 +0200

    open options refactor

--- html/js/project-editor/openOptions.js
+++ html/js/project-editor/openOptions.js
@@ -11,21 +11,3 @@ const expandOptions = (source) => {
         options.style.display = "flex";
     };
 };
-
-const checkCheckbox = (source) => {
-    const checkbox = source.querySelector("input[type='checkbox']");
-    checkbox.checked = !checkbox.checked;
-};
-
-const checkboxAreas = document.querySelectorAll(".block .options div div");
-
-checkboxAreas.forEach(checkbox => {
-    checkbox.addEventListener("click", () => {
-        checkCheckbox(checkbox);
-    });
-    checkbox.addEventListener("keydown", (event) => {
-        if (event.key === "Enter" || event.key === " ") {
-            checkCheckbox(checkbox);
-        };
-    });
-});
