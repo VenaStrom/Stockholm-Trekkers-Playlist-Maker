95c53e67d28557805805be797abb26fbb28115a3
viggoStrom <viggostrom05@gmail.com>
2024-09-17 20:03:27 +0200

    fixed clickable elements

--- html/js/clickableElements.js
+++ html/js/clickableElements.js
@@ -1,10 +1,38 @@
 
+
 document.addEventListener("keydown", (event) => {
     if (!(event.key === "Enter" || event.key === " ")) {
         return;
     };
+    
+    // Prevents the page from scrolling when pressing the spacebar
+    if (event.key === " ") { event.preventDefault() };
+
     if (event.target.classList.contains("clickable")) {
-        event.target.click();
-        event.target.querySelector("&>input").click();
+        if (event.target.hasAttribute("onclick")) { event.target.click() };
+        if (event.target.querySelector("input")) { event.target.querySelector("input").click() };
+    };
+});
+
+document.addEventListener("click", (event) => {
+    const target = event.target;
+    const parent = event.target.parentElement;
+
+    if (parent.classList.contains("clickable")) {
+        if (
+            target.tagName === "P"
+            &&
+            parent.querySelector("input")
+        ) {
+            parent.querySelector("input").click();
+        };
+    } else if (target.classList.contains("clickable")) {
+        if (
+            target.querySelector("p")
+            &&
+            target.querySelector("input")
+        ) {
+            target.querySelector("input").click();
+        };
     };
 });
