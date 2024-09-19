4e962deb026762dc3367bc6006aa94ee9a378d71
viggoStrom <viggostrom05@gmail.com>
2024-09-19 08:14:40 +0200

    meta data testing, will branch

--- scripts/getMetaData.js
+++ scripts/getMetaData.js
@@ -1,11 +1,12 @@
 const { ipcMain } = require("electron");
 const fs = require("fs");
 const path = require("node:path")
-const raiseError = require("../raiseError.js");
+const raiseError = require("./raiseError.js");
 
 
 const getMetaData = (filePath) => {
-    
+    const file = fs.statSync(filePath);
+    console.log(file);
 };
 
 const setUpHandlers = () => {
@@ -13,3 +14,6 @@ const setUpHandlers = () => {
 };
 
 module.exports = { setUpHandlers };
+
+console.log("[DEBUG] getMetaData.js loaded");
+getMetaData("assets/videos/pause_1_min_covid.mp4");
