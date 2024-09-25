c64a382b9300851f1b09daf122627189b66cbafd
viggoStrom <viggostrom05@gmail.com>
2024-09-25 19:38:52 +0200

    confirm reload

--- html/js/project-editor/backButton.js
+++ html/js/project-editor/backButton.js
@@ -9,3 +9,15 @@ backButton.addEventListener("click", () => {
         window.location.href = "./projects.html";
     }
 });
+
+
+// Confirm on unsaved refresh
+document.addEventListener("keydown", (event) => {
+    if (event.ctrlKey && event.key === "r") {
+        if (document.querySelector("header #save-status").textContent.includes("*")) {
+            if (!confirm("You have unsaved changes. Are you sure you want to refresh?")) {
+                event.preventDefault();
+            }
+        }
+    }
+});
