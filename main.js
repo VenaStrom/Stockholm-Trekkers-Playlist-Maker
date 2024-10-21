daba3e32cdb475e64f649b08c6dc7e1d9e6d450c
viggoStrom <viggostrom05@gmail.com>
2024-10-21 08:08:03 +0200

    commented main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const { app, BrowserWindow, nativeTheme } = require("electron");
 const path = require("node:path");
 const { setUpHandlers: setUpDownloadHandlers } = require("./scripts/download/downloadAssets.js");
@@ -6,8 +5,7 @@ const { setUpHandlers: setUpProjectHandlers } = require("./scripts/save/projects
 const { setUpHandlers: setUpMetadataHandlers } = require("./scripts/getMetaData.js");
 const { setUpHandlers: setUpExportHandlers } = require("./scripts/export.js");
 
-const createWindow = () => {
-
+const createMainWindow = () => {
     const mainWindow = new BrowserWindow({
         width: 1920,
         height: 1080,
@@ -20,24 +18,25 @@ const createWindow = () => {
         titleBarOverlay: { color: "#1e1e1e", symbolColor: "#f2f2f2" },
     });
 
-    // mainWindow.openDevTools();
-
-
+    // When closing the main window, close all other windows (i.e. hidden download windows)
     mainWindow.on("closed", () => {
         BrowserWindow.getAllWindows().forEach(window => {
             window.close();
         });
     });
 
-    mainWindow.maximize();
+    // Load and show the main window at the download assets page
     mainWindow.loadFile("./html/pages/download.html");
+    mainWindow.maximize();
     mainWindow.focus();
 };
 
 app.whenReady().then(() => {
 
-    createWindow();
+    createMainWindow();
 
+    // Set up the handlers for the ipc messages
+    // The handlers are defined in the respective files
     setUpDownloadHandlers();
     setUpProjectHandlers();
     setUpMetadataHandlers();
@@ -45,11 +44,13 @@ app.whenReady().then(() => {
 
     console.log("[INFO] handlers set up");
 
+    // Just in case something goes wrong, make the main window if it somehow didn't get made
     app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+        if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
     });
 });
 
+// Quit on macOS when all windows are closed due to how mac behaves with windows
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") app.quit();
 });
