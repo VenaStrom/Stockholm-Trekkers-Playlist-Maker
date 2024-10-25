534f851b3fc2006933594d8bfb8f8649e8689078
viggoStrom <viggostrom05@gmail.com>
2024-10-25 09:34:41 +0200

    moved export function to worker thread file

--- scripts/export.js
+++ scripts/export.js
@@ -1,8 +1,7 @@
 const { ipcMain, dialog } = require("electron");
-const { Worker, parentPort } = require("worker_threads");
+const { Worker } = require("worker_threads");
 const path = require("node:path");
 const fs = require("node:fs");
-const raiseError = require("./raiseError.js");
 const { projectGet, projectFolder: userData } = require("./save/projects.js");
 
 let copyWorker;
@@ -12,64 +11,6 @@ const exportStatus = {
     progress: "0%",
     message: "Making folders...",
     exportLocation: "",
-};
-
-const copyAllAssets = (projectJSON, exportFolder) => {
-    // Copies the pause clips to the output folder
-    const pauseFolder = path.join(__dirname, "..", "assets", "videos");
-
-    // Update export status
-    exportStatus.type = "status"; // This is added so the parent thread knows how to handle the post
-    exportStatus.message = "Copying assets...";
-    exportStatus.progress = "15%";
-    parentPort.postMessage(exportStatus);
-
-    // Copy the entire pauses folder to the output folder in /pauses
-    console.log("[INFO] Copying pauses...");
-    fs.mkdirSync(path.join(exportFolder, "pauses"));
-    fs.cpSync(pauseFolder, path.join(exportFolder, "pauses"), { recursive: true });
-
-    // Make episodes folder
-    fs.mkdirSync(path.join(exportFolder, "episodes"));
-
-    // Update export status
-    exportStatus.message = "Copying episodes...";
-    exportStatus.progress = "20%";
-    parentPort.postMessage(exportStatus);
-
-    // Loop through all the episodes and copy them from their given path to the output folder
-    console.log("[INFO] Copying episodes...");
-    projectJSON.blocks.forEach((block, blockIndex) => {
-        block.episodes.forEach((episode, episodeIndex) => {
-
-            // Update export status
-            exportStatus.progress = `${20 + (80 / projectJSON.blocks.length / block.episodes.length) * (blockIndex * block.episodes.length + episodeIndex)}%`; // Gets a percent inbetween 20 and 100 based on which episode and block you are on
-            parentPort.postMessage(exportStatus);
-
-            // Check if the file path is missing or if the file is missing
-            if (episode.filePath === "") { raiseError("Cannot find " + episode.fileName + ". The file path seems to be missing"); return; };
-            if (!fs.existsSync(episode.filePath)) { raiseError("Cannot find " + episode.fileName + ". The file seems to be missing"); return; };
-
-            // If a file with the same name already exists in the output folder, don't copy again
-            if (fs.existsSync(path.join(exportFolder, "episodes", episode.fileName))) {
-                console.log(`[WARN] A file (${episode.fileName}) wasn't copied due to the project already containing a file with the same name. This should be expected behavior when showing multiples of the same file but it could also be bad if files accidentally were named the same thing.`);
-                return;
-            };
-
-            // Copy the episode to the output folder
-            fs.copyFileSync(episode.filePath, path.join(exportFolder, "episodes", episode.fileName));
-        });
-    });
-
-    // Copy the save file from user data to the output folder
-    console.log("[INFO] Copying save file...");
-    fs.mkdirSync(path.join(exportFolder, "project-save-file"));
-    fs.copyFileSync(path.join(userData, projectJSON.id + ".json"), path.join(exportFolder, "project-save-file", projectJSON.id + ".json"));
-
-    // Update export status
-    exportStatus.message = "Done!";
-    exportStatus.progress = "100%";
-    parentPort.postMessage(exportStatus);
 };
 
 // Make the ps1 "harness" that runs VLC and runs the correct episodes at the correct times
@@ -339,15 +280,17 @@ Directly exporting to a USB drive is slow and can cause the program to hang.`
     // Make the ps1 script
     makePS1(projectJSON, exportLocation);
 
-    // Makes a worker thread to copy all the assets and replay its status to the main thread
+    // Makes a worker thread to copy all the assets and relay its status to the main thread
     copyWorker = new Worker(path.join(__dirname, "workerExport.js"));
     copyWorker.on("message", (message) => { // This updates the export status in the main scope
         if (message.type === "status") {
             exportStatus.message = message.message;
             exportStatus.progress = message.progress;
+            console.log("[INFO] " + message.progress + " - " + message.message);
         }
     });
-    copyWorker.postMessage({ projectJSON, projectFolder: exportLocation }); // This starts the copying
+    // This starts the copying
+    copyWorker.postMessage({ projectJSON, projectFolder: exportLocation, userData });
 };
 
 
@@ -374,4 +317,4 @@ const setUpHandlers = () => {
     });
 };
 
-module.exports = { setUpHandlers, copyAllAssets };
+module.exports = { setUpHandlers };
--- scripts/workerExport.js
+++ scripts/workerExport.js
@@ -1,8 +1,69 @@
 const { parentPort } = require("worker_threads");
-// const { copyAllAssets } = require("./export.js");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const exportStatus = {};
+
+const copyAllAssets = (projectJSON, exportFolder, userData) => {
+    // Copies the pause clips to the output folder
+    const pauseFolder = path.join(__dirname, "..", "assets", "videos");
+
+    // Update export status
+    exportStatus.type = "status"; // This is added so the parent thread knows how to handle the post
+    exportStatus.message = "Copying assets...";
+    exportStatus.progress = "15%";
+    parentPort.postMessage(exportStatus);
+
+    // Copy the entire pauses folder to the output folder in /pauses
+    console.log("[INFO] Copying pauses...");
+    fs.mkdirSync(path.join(exportFolder, "pauses"));
+    fs.cpSync(pauseFolder, path.join(exportFolder, "pauses"), { recursive: true });
+
+    // Make episodes folder
+    fs.mkdirSync(path.join(exportFolder, "episodes"));
+
+    // Update export status
+    exportStatus.message = "Copying episodes...";
+    exportStatus.progress = "20%";
+    parentPort.postMessage(exportStatus);
+
+    // Loop through all the episodes and copy them from their given path to the output folder
+    console.log("[INFO] Copying episodes...");
+    projectJSON.blocks.forEach((block, blockIndex) => {
+        block.episodes.forEach((episode, episodeIndex) => {
+
+            // Update export status
+            exportStatus.progress = `${20 + (80 / projectJSON.blocks.length / block.episodes.length) * (blockIndex * block.episodes.length + episodeIndex)}%`; // Gets a percent inbetween 20 and 100 based on which episode and block you are on
+            parentPort.postMessage(exportStatus);
+
+            // Check if the file path is missing or if the file is missing
+            if (episode.filePath === "") { raiseError("Cannot find " + episode.fileName + ". The file path seems to be missing"); return; };
+            if (!fs.existsSync(episode.filePath)) { raiseError("Cannot find " + episode.fileName + ". The file seems to be missing"); return; };
+
+            // If a file with the same name already exists in the output folder, don't copy again
+            if (fs.existsSync(path.join(exportFolder, "episodes", episode.fileName))) {
+                console.log(`[WARN] A file (${episode.fileName}) wasn't copied due to the project already containing a file with the same name. This should be expected behavior when showing multiples of the same file but it could also be bad if files accidentally were named the same thing.`);
+                return;
+            };
+
+            // Copy the episode to the output folder
+            fs.copyFileSync(episode.filePath, path.join(exportFolder, "episodes", episode.fileName));
+        });
+    });
+
+    // Copy the save file from user data to the output folder
+    console.log("[INFO] Copying save file...");
+    fs.mkdirSync(path.join(exportFolder, "project-save-file"));
+    fs.copyFileSync(path.join(userData, projectJSON.id + ".json"), path.join(exportFolder, "project-save-file", projectJSON.id + ".json"));
+
+    // Update export status
+    exportStatus.message = "Done!";
+    exportStatus.progress = "100%";
+    parentPort.postMessage(exportStatus);
+};
+
 
 parentPort.on("message", (message) => {
     console.log("[INFO] Worker started working");
-    console.log(message);
-    // copyAllAssets(message.projectJSON, message.projectFolder);
+    copyAllAssets(message.projectJSON, message.projectFolder, message.userData);
 });
