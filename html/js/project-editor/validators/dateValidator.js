dd1313847946ec7ebaa8fdd48652d64ed29dbaa0
viggoStrom <viggostrom05@gmail.com>
2024-10-29 23:00:56 +0100

    changed date validator messages

--- html/js/project-editor/validators/dateValidator.js
+++ html/js/project-editor/validators/dateValidator.js
@@ -6,21 +6,21 @@ const dateValidator = (inputDate) => {
     const warnings = [];
 
     if (date == "Invalid Date") {
-        warnings.push("It's not a valid date. ");
+        warnings.push("That's not a valid date.");
 
         return warnings; // There's no point in checking further if the date is invalid since the rest of the checks rely on the date object
     };
     if (!(date.getDay() === 0 || date.getDay() === 6)) {
-        warnings.push("It's on a weekday. ");
+        warnings.push("That's on a weekday. Are you sure?");
     };
     if (date.getFullYear() !== new Date().getFullYear()) {
-        warnings.push("It's not this year. ");
+        warnings.push("That's not the current year. Are you sure?");
     };
     if (date < new Date()) {
-        warnings.push("It's in the past. ");
+        warnings.push("That's in the past.");
     };
-    if (date.getTime() > new Date().getTime() + 5184000000) {
-        warnings.push("It's more than 2 months away. ");
+    if (date.getTime() > new Date().getTime() + 2592000000) {
+        warnings.push("That's more than 1 month away. Are you sure?");
     };
 
     return warnings;
