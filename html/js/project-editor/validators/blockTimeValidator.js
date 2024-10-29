aee801d4e42157679b526ff5b24e51a59c40c0c7
viggoStrom <viggostrom05@gmail.com>
2024-10-29 22:55:14 +0100

    broke out block time validation to a separate function

--- html/js/project-editor/validators/blockTimeValidator.js
+++ html/js/project-editor/validators/blockTimeValidator.js
@@ -2,15 +2,16 @@
 const earliestTime = 9;
 const latestTime = 20;
 
-const blockTimeValidator = (event) => {
+const validateTime = (time) => {
     const warnings = [];
 
-    const time = event.target.value;
     const [hours, minutes] = time.split(":").map(Number);
 
-    // Empty time
+    // Empty or invalid time
     if (time === "") {
         warnings.push("Time is empty.");
+
+        return warnings; // No need to check further
     }
     if (
         time.length !== 5
@@ -20,14 +21,20 @@ const blockTimeValidator = (event) => {
         isNaN(hours)
     ) {
         warnings.push("Time must be in the format HH:MM.");
+
+        return warnings; // No need to check further
     }
 
     // Standard time format checks
     if (hours < 0 || hours > 23) {
         warnings.push("Hours must be between 0 and 23.");
+
+        return warnings; // No need to check further
     }
     if (minutes < 0 || minutes > 59) {
         warnings.push("Minutes must be between 0 and 59.");
+
+        return warnings; // No need to check further
     }
 
     // Is it a bit too early?
@@ -51,6 +58,18 @@ const blockTimeValidator = (event) => {
         warnings.push("Duplicate time.");
     }
 
+
+
+    return warnings;
+};
+
+const blockTimeValidator = (event) => {
+
+    const time = event.target.value;
+    const warnings = validateTime(time);
+
+    const blocks = document.querySelectorAll(".block");
+    const blockTimes = Array.from(blocks).map(block => block.querySelector(".time input[type='text']").value);
     const blockIndex = blockTimes.indexOf(time);
     const id = "block-time-warning-popup-id" + blockIndex;
     warningPopup(id, event.target, warnings);
