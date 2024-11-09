0b487bcb7de7b1672a26d84327a946ef08ad8c64
viggoStrom <viggostrom05@gmail.com>
2024-11-09 19:34:40 +0100

    made the save status text clickable again to save

--- html/js/playlist-editor/save.js
+++ html/js/playlist-editor/save.js
@@ -2,9 +2,12 @@
 // For now, auto saving is flat out disabled. You will have to save manually
 const autoSave = false;
 
+
+// Return the entire project as a JSON object
 const getJSONstruct = () => {
     const date = document.querySelector(".date-input input[type='text']").value;
 
+    // Get all the DOM objects of the blocks that are not hidden
     const blocks = document.querySelectorAll(".block:not(.hidden)");
 
     const struct = {
@@ -14,7 +17,7 @@ const getJSONstruct = () => {
         blocks: [],
     };
 
-
+    // Loop through all the blocks and get the options and episodes and append them to the struct
     blocks.forEach((block) => {
 
         // Get the options of the block and set them in the struct
@@ -30,7 +33,8 @@ const getJSONstruct = () => {
 
         // Loop through and export all the episodes as a list
         const episodes = Array.from(block.querySelectorAll(".episode:not(.hidden)")) // only grab non-hidden episodes
-            .filter((episode) => { // remove the file inputs that are empty
+            .filter((episode) => { 
+                // ignore the file inputs that are empty
                 const fileInput = episode.querySelector("input[type='file']");
                 if (fileInput.value !== "") { return true; };
 
@@ -38,7 +42,7 @@ const getJSONstruct = () => {
                 const fileInput = episode.querySelector("input[type='file']");
                 return {
                     filePath: fileInput.getAttribute("data-file-path"),
-                    fileName: fileInput.value.split(/[/\\]/).at(-1), // split path via / or \ and get the last element which should be the file name with it's extension
+                    fileName: fileInput.value.split(/[/\\]/).at(-1), // a somewhat hacky way to get the file name from the path
                     startTime: episode.querySelector(".time p").textContent,
                     endTime: episode.querySelector(".time p").dataset.endTime,
                     duration: episode.querySelector(".time p").dataset.duration,
@@ -66,6 +70,9 @@ const saveProject = () => {
     });
 };
 
+
+// Actions leading to saving //
+
 // Save on the export button and start the export
 const exportButton = document.querySelector("button.export");
 exportButton.addEventListener("click", () => {
@@ -88,11 +95,16 @@ document.addEventListener("keydown", (event) => {
     saveProject();
 });
 
-// Save project on change [Will never save, see top of file]
+// Save project on change if autoSave is enabled
 document.addEventListener("change", () => {
     saveStatusText.textContent = "Latest changes not saved*";
 
     if (autoSave) {
         saveProject();
     }
+});
+
+// Save on clicking the save status text
+saveStatusText.addEventListener("click", () => {
+    saveProject();
 });
