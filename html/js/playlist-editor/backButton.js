12e1b06815ab4fd2ffb482d7c15321202342f074
viggoStrom <viggostrom05@gmail.com>
2024-11-09 19:49:49 +0100

    changed saving behavior when using the back button. Refactored some code in the backbutton file

--- html/js/playlist-editor/backButton.js
+++ html/js/playlist-editor/backButton.js
@@ -7,8 +7,8 @@ const confirmUnsavedChanges = () => {
 };
 
 // Is it not saved?
-const isUnsaved = () => {
-    return document.querySelector("header #save-status").textContent.includes("*");
+const isSaved = () => {
+    return !document.querySelector("header #save-status").textContent.includes("*");
 };
 
 // Are we exporting?
@@ -16,10 +16,24 @@ const isExporting = () => {
     return !document.querySelector(".export-progress-window").classList.contains("hidden");
 };
 
+// Ask to save if the user has unsaved changes
+const askToSave = () => {
+    return confirm("You have unsaved changes. Do you want to save? You will not leave this page.");
+};
+
+
 // When clicking back button, confirm that the user actually wants to leave
 backButton.addEventListener("click", () => {
+
+    if (!isSaved()) {
+        if (askToSave()) {
+            saveProject();
+            return; // Don't leave the page
+        }
+    }
+
     if (
-        isUnsaved()
+        !isSaved()
         ||
         isExporting()
     ) {
@@ -32,34 +46,23 @@ backButton.addEventListener("click", () => {
         };
     } else {
         // Go back to the projects page
+        // This is the default action 
         window.location.href = "./projects.html";
     };
 });
 
 // Ctrl + R confirmation to prevent unwanted refresh
-document.addEventListener("keydown", (event) => {
-    if (
-        (event.ctrlKey && event.key === "r")
-        &&
-        (
-            isUnsaved()
-            ||
-            isExporting()
-        )
-        &&
-        !confirmUnsavedChanges()
-    ) {
-        event.preventDefault();
-    };
-});
-
 // Ctrl + W confirmation to prevent unwanted close
 document.addEventListener("keydown", (event) => {
     if (
-        (event.ctrlKey && event.key === "w")
+        (
+            (event.ctrlKey && event.key === "r")
+            ||
+            (event.ctrlKey && event.key === "w")
+        )
         &&
         (
-            isUnsaved()
+            !isSaved()
             ||
             isExporting()
         )
