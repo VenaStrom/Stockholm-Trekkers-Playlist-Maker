7cb1466db69e666f7318d11fde48ea3fa6290f3e
viggoStrom <viggostrom05@gmail.com>
2024-11-10 19:34:01 +0100

    added a button to open the folder where the project files are saved

--- html/js/importFromFile.js
+++ html/js/importFromFile.js
@@ -1,7 +1,18 @@
 
+const importButton = document.querySelector(".import-button");
+const openButton = document.querySelector(".open-folder-button");
+
 const importFromFile = () => {
     importer.import().then((data) => {
-        console.log(data);
         window.location.reload();
     });
 };
+importButton.addEventListener("click", importFromFile);
+
+
+const openSavesFolder = () => {
+    appPath.get().then((path) => {
+        explorer.open(path + "/user-data/projects");
+    });
+};
+openButton.addEventListener("click", openSavesFolder);
