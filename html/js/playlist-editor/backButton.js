5813466d60cde021d4b01e551448eeff69114bb7
viggoStrom <viggostrom05@gmail.com>
2024-12-29 12:24:27 +0100

    reimplemented the back button in the editor

--- html/js/playlist-editor/backButton.js
+++ html/js/playlist-editor/backButton.js
@@ -1,4 +1,25 @@
 
-document.querySelector(".back-button").addEventListener("click", () => {
+const wantsToLeave = () => {
+    return confirm("You have unsaved changes. Are you sure you want to leave?");
+};
+
+const leavePage = () => {
     window.location.href = "./projects-page.html";
+};
+
+
+document.querySelector(".back-button").addEventListener("click", () => {
+    if (
+        isUnsaved()
+        &&
+        wantsToLeave()
+    ) {
+        leavePage();
+        return;
+    }
+
+    if (isSaved()) {
+        leavePage();
+        return;
+    }
 });
