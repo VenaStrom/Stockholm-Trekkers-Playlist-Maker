752510d99215834c48dbce7616e277813c059652
viggoStrom <viggostrom05@gmail.com>
2025-01-04 15:05:20 +0100

    changed where files downloaded and generated files are stored

--- filePaths.js
+++ filePaths.js
@@ -2,16 +2,21 @@
 
 const path = require("node:path");
 const fs = require("node:fs");
+const os = require("node:os");
 
+const appName = JSON.parse(fs.readFileSync(path.join(__dirname, "package.json"))).name;
 
-const userDataFolder = path.join(path.resolve(__dirname), "user-data");
-if (!fs.existsSync(userDataFolder)) fs.mkdirSync(userDataFolder);
+const externalDataFolder = path.join(os.homedir(), appName);
+if (!fs.existsSync(externalDataFolder)) fs.mkdirSync(externalDataFolder, { recursive: true });
+
+const userDataFolder = path.join(externalDataFolder, "user-data");
+if (!fs.existsSync(userDataFolder)) fs.mkdirSync(userDataFolder, { recursive: true });
 
 const saveFilesFolder = path.join(userDataFolder, "save-files");
-if (!fs.existsSync(saveFilesFolder)) fs.mkdirSync(saveFilesFolder);
+if (!fs.existsSync(saveFilesFolder)) fs.mkdirSync(saveFilesFolder, { recursive: true });
 
-const videoAssetsFolder = path.join(__dirname, "assets", "videos");
-if (!fs.existsSync(videoAssetsFolder)) fs.mkdirSync(videoAssetsFolder);
+const videoAssetsFolder = path.join(externalDataFolder, "assets");
+if (!fs.existsSync(videoAssetsFolder)) fs.mkdirSync(videoAssetsFolder, { recursive: true });
 
 const downloadReferenceFile = path.join(__dirname, "assetDownloadInfo.json");
 
