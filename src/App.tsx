3e712e3f0d50f90725bf39eacba7568b7311d428
Vena <strom.vena+gh@gmail.com>
2025-10-21 14:52:57 +0200

    move assets

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,25 @@ export default function App() {
     setHasSetListeners(true);
   });
 
-  return (
-    <main className="">
+  return (<>
+    <header className="">
+      <img className="size-18" src="/icon/org/stockholm-trekkers-256x256.png" alt="Stockholm Trekkers Logo" />
+      <p className="flex flex-row gap-x-2">
+        <span>Stockholm Trekkers</span>
+        <span>Playlist Maker</span>
+      </p>
+    </header>
 
+    <main className="">
+      p
     </main>
+
+    <footer className="flex flex-row justify-center items-center">
+      <p className="flex flex-col">
+        <span>Made by: <a href="https://venastrom.se" target="_blank" rel="noreferrer">Vena Ström</a></span>
+        <span>Contact: <a href="mailto:strom.vena+playlistmaker@gmail.com?subject=Playlist%20Maker" target="_blank" rel="noreferrer">strom.vena@gmail.com</a></span>
+      </p>
+    </footer>
+  </>
   );
 }
