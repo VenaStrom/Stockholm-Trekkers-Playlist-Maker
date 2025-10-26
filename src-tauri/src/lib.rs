ee655613a5eefe16da9a4d46549d11ac6246d6a6
Vena <strom.vena+gh@gmail.com>
2025-10-26 23:54:14 +0100

    remove opener plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,7 +12,6 @@ async fn delete_project(project_id: String) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .plugin(tauri_plugin_opener::init())
     .invoke_handler(tauri::generate_handler![close])
     .invoke_handler(tauri::generate_handler![delete_project])
     .run(tauri::generate_context!())
