214e07bbb6de6e6b626da20286c9e8ce1196c3da
Vena <strom.vena+gh@gmail.com>
2025-11-12 23:24:26 +0100

    update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,12 +4,25 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
+
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
-    files: ["**/*.{ts,mts,cts,tsx,js,jsx}"],
+    settings: {
+      "react": { version: "detect" },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat["recommended"],
+  pluginReact.configs.flat["jsx-runtime"],
+  // Custom rules for TS files
+  {
+    files: ["**/*.{ts,mts,cts,tsx}"],
     rules: {
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_', 'caughtErrorsIgnorePattern': '^_' }],
