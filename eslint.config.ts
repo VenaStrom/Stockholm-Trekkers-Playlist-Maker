93b4596dc0b837f85bb4d5263d5fae3c7233e08d
Vena <strom.vena+gh@gmail.com>
2025-11-16 16:27:53 +0100

    WIP eslint config update

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,11 +7,12 @@ import { defineConfig, globalIgnores } from "eslint/config";
 export default defineConfig([
   globalIgnores([
     "**/src-tauri/**",
+    "**/node_modules/**",
+    "**/dist/**",
+    "**/build/**",
   ]),
   {
-    settings: {
-      "react": { version: "detect" },
-    },
+    settings: { "react": { version: "detect" }, },
   },
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
@@ -20,15 +21,18 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
+  tseslint.configs.eslintRecommended,
   pluginReact.configs.flat["recommended"],
   pluginReact.configs.flat["jsx-runtime"],
   // Custom rules for TS files
   {
     files: ["**/*.{ts,mts,cts,tsx}"],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'error',
+      // No unused vars
       '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_', 'caughtErrorsIgnorePattern': '^_' }],
-      'react/react-in-jsx-scope': 'off',
+
+      // No explicit any
+      '@typescript-eslint/no-explicit-any': 'error',
     },
   },
 ]);
