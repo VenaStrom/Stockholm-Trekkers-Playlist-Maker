77497224c63c7407c3a495aa417889427ea32226
Vena <strom.vena+gh@gmail.com>
2025-11-16 17:02:33 +0100

    narrow file inclusion in linting

--- eslint.config.ts
+++ eslint.config.ts
@@ -10,7 +10,7 @@ export default defineConfig([
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ["src/**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2022,
       parser: tseslint.parser,
