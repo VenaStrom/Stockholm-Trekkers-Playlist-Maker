2726715d82a84dd9dc32c3c578ae4ca9b0deeb51
VenaStrom <strom.vena+gh@gmail.com>
2025-12-27 14:02:54 +0100

    add dupe episode and block checks on save

--- src/functions/project/save-project.ts
+++ src/functions/project/save-project.ts
@@ -2,12 +2,54 @@ import * as fs from "@tauri-apps/plugin-fs";
 import { path } from "@tauri-apps/api";
 import { PathName, FileName } from "@/global";
 import { isProject } from "../type-guards";
-import type { Project, ProjectData, ProjectMeta } from "@/types";
+import type { Episode, Project, ProjectData, ProjectMeta } from "@/types";
 import { createProject } from "./create-project";
 
 export async function saveProject(project: Project): Promise<void> {
   if (!isProject(project)) {
     throw new Error("Project data is invalid and cannot be saved.");
+  }
+
+  // If dupe episodes or blocks exist, warn and remove the dupe with the least data
+  const episodesById:Record<string, Episode[]> = {};
+  project.episodes.forEach(e => {
+    const id = e.id;
+    episodesById[id] ??= [];
+    episodesById[id].push(e);
+  });
+  for (const [id, episodes] of Object.entries(episodesById)) {
+    if (episodes.length > 1) {
+      console.warn(`Duplicate episodes found with id ${id}. Keeping the episode with the most data.`);
+      episodes.sort((a, b) => {
+        const aDataCount = Object.values(a).filter(v => v).length;
+        const bDataCount = Object.values(b).filter(v => v).length;
+        return bDataCount - aDataCount;
+      });
+      const [_keep, ...dupes] = episodes;
+      project.episodes = project.episodes.filter(e => e !== dupes[0]);
+    }
+  }
+  const blocksById:Record<string, number> = {};
+  project.blocks.forEach(b => {
+    const id = b.id;
+    blocksById[id] ??= 0;
+    blocksById[id]++;
+  });
+  for (const [id, count] of Object.entries(blocksById)) {
+    if (count > 1) {
+      console.warn(`Duplicate blocks found with id ${id}. Removing duplicates.`);
+      let firstFound = false;
+      project.blocks = project.blocks.filter(b => {
+        if (b.id === id) {
+          if (!firstFound) {
+            firstFound = true;
+            return true;
+          }
+          return false;
+        }
+        return true;
+      });
+    }
   }
 
   const projectDir = await path.join(PathName.UserProjectsDir, project.id);
@@ -20,7 +62,8 @@ export async function saveProject(project: Project): Promise<void> {
 
   // Used for meta stats, not to override the actual data
   const truthyEpisodes = project.episodes.filter(e => e.filePath);
-  const blocksWithEpisodes = project.blocks.filter(b => truthyEpisodes.some(e => e.blockId === b.id));
+  // const blocksWithEpisodes = project.blocks.filter(b => truthyEpisodes.some(e => e.blockId === b.id));
+  const blocksWithEpisodes = project.blocks; // This will include empty blocks since they are structurally more important than episodes
 
   const projectMeta: ProjectMeta = {
     id: project.id,
